101df863681d59e0f2d58be061198526eef82a08
Adit Arya <aryaadit@hotmail.com>
2025-12-04 00:22:14 -0500

    Mobile comment out server config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.pourdecisions.barkeeply',
   appName: 'barkeeply',
   webDir: 'dist',
-  server: {
-    url: 'https://83dab526-5dce-4cd6-b467-9c007e9792e6.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  // server: {
+  //   url: 'https://83dab526-5dce-4cd6-b467-9c007e9792e6.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true
+  // }
 };
 
 export default config;
